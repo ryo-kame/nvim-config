613a649f1faa7fabd5ebbcb513d23023545a802b
Ryosuke Kamei <ryosukekamei@RyosukenoMacBook-Air.local>
2026-06-01 22:10:23 +0900

    fix: set lualine theme to catppuccin-mocha to match flavour

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,7 +5,7 @@ return {
   config = function()
     require("lualine").setup({
       options = {
-        theme = "catppuccin", -- 好きなテーマに変えてね
+        theme = "catppuccin-mocha", -- catppuccin の flavour に合わせる（catppuccin-nvim で自動追従も可）
         icons_enabled = true,
         section_separators = { left = '', right = '' },
         component_separators = { left = '', right = '' },
